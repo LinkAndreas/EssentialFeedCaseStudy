7f5bb878eeb5e2923b096a10209a79e8feffa66c
Andreas Link <mail@linkandreas.de>
2021-07-06 20:49:50 +0200

    Add NSMaanagedObjectModel and NSPersistentContainer convenience initializers

---
+++ EssentialFeed/Feed Cache/Helper/NSManagedObjectModel+initWithName.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2021 Andreas Link. All rights reserved.
+
+import CoreData
+
+extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
+}
