f0a148fcbf2e80ca1f4b9d04d4cae80543290bf7
Andreas Link <mail@linkandreas.de>
2021-07-18 19:21:15 +0200

    Extract sut creation into factory method and add memory leak tracking

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -21,15 +21,13 @@ final class FeedViewController: UIViewController {
 
 final class FeedViewControllerTests: XCTestCase {
     func test_init_doesNotLoadFeed() {
-        let loader = LoaderSpy()
-        _  = FeedViewController(loader: loader)
+        let (loader, _) = makeSUT()
 
         XCTAssertEqual(loader.callCount, 0)
     }
 
     func test_viewDidLoad_loadsFeed() {
-        let loader = LoaderSpy()
-        let sut = FeedViewController(loader: loader)
+        let (loader, sut) = makeSUT()
 
         sut.loadViewIfNeeded()
 
@@ -37,6 +35,17 @@ final class FeedViewControllerTests: XCTestCase {
     }
 
     // MARK: - Helpers
+    private func makeSUT(
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (loader: LoaderSpy, sut: FeedViewController) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (loader, sut)
+    }
+
     final class LoaderSpy: FeedLoader {
         private (set) var callCount = 0
 
