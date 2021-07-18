cc93793a1a6f37e898b4f68bafca50a4f621e98c
Andreas Link <mail@linkandreas.de>
2021-07-18 19:43:11 +0200

    viewDidLoad shows loading indicator

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -3,7 +3,7 @@
 import EssentialFeed
 import XCTest
 
-final class FeedViewController: UIViewController {
+final class FeedViewController: UITableViewController {
     private var loader: FeedLoader?
 
     convenience init(loader: FeedLoader) {
@@ -15,6 +15,14 @@ final class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load ), for: .valueChanged)
+        load()
+    }
+
+    @objc
+    private func load() {
+        refreshControl?.beginRefreshing()
         loader?.fetchFeed { _ in }
     }
 }
@@ -34,6 +42,24 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.callCount, 1)
     }
 
+    func test_pullToRefresh_loadsFeed() {
+        let (loader, sut) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        sut.refreshControl?.simulatePullToRefresh()
+        sut.refreshControl?.simulatePullToRefresh()
+
+        XCTAssertEqual(loader.callCount, 3)
+    }
+
+    func test_viewDidLoad_showsLoadingIndicator() {
+        let (loader, sut) = makeSUT()
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+    }
+
     // MARK: - Helpers
     private func makeSUT(
         file: StaticString = #filePath,
@@ -51,6 +77,16 @@ final class FeedViewControllerTests: XCTestCase {
 
         func fetchFeed(completion: @escaping (FeedLoader.Result) -> Void) {
             callCount += 1
+        }
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
         }
     }
 }
