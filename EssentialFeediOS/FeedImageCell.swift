bbd2e0a41d6b64bb98fead52f9d49a8c473b1ed6
Andreas Link <mail@linkandreas.de>
2021-07-20 19:30:07 +0200

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,9 @@
+//  Copyright © 2021 Andreas Link. All rights reserved.
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationLabel: UILabel = .init()
+    public let descriptionLabel: UILabel = .init()
+    public let locationContainer: UIView = .init()
+}
