588cc0651a198dbdd2f97b88debf78d317e90919
Andreas Link <mail@linkandreas.de>
2021-08-24 22:07:54 +0200

    Add UITableView helper to simplify cell dequeuing

---
+++ EssentialFeediOS/Views/UITableView+Dequeuing.swift
@@ -0,0 +1,10 @@
+//  Copyright © 2021 Andreas Link. All rights reserved.
+
+import UIKit
+
+extension UITableView {
+    func dequeueReusableCell<T: UITableViewCell>() -> T {
+        let identifier = String(describing: T.self)
+        return dequeueReusableCell(withIdentifier: identifier) as! T
+    }
+}
