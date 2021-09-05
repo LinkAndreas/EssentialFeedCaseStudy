da1d786d4b28c7d5adf5474a05d74c9e8e50c14b
Andreas Link <andreas.link@jamitlabs.com>
2021-09-05 09:07:17 +0200

    Extract FeedUIIntegrationTest test helpers into separate files

---
+++ EssentialFeediOSTests/Helpers/FeedViewController+TestHelpers.swift
@@ -0,0 +1,56 @@
+//  Copyright © 2021 Andreas Link. All rights reserved.
+
+import EssentialFeediOS
+import UIKit
+
+extension FeedViewController {
+    func simulateUserInitiatedFeedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+
+    @discardableResult
+    func simulateFeedImageViewVisible(atIndex index: Int = 0) -> FeedImageCell? {
+        return feedImageView(atIndex: index) as? FeedImageCell
+    }
+
+    @discardableResult
+    func simulateFeedImageViewNotVisible(atIndex index: Int = 0) -> FeedImageCell? {
+        let view = simulateFeedImageViewVisible(atIndex: index)
+
+        let delegate = tableView.delegate
+        delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: .init(row: index, section: 0))
+        return view
+    }
+
+    func simulateFeedImageViewNearVisible(atIndex index: Int = 0) {
+        let dataSource = tableView.prefetchDataSource
+        let indexPath = IndexPath(row: index, section: 0)
+        dataSource?.tableView(tableView, prefetchRowsAt: [indexPath])
+    }
+
+    func simulateFeedImageViewNotNearVisible(atIndex index: Int = 0) {
+        simulateFeedImageViewNearVisible(atIndex: index)
+
+        let dataSource = tableView.prefetchDataSource
+        let indexPath = IndexPath(row: index, section: 0)
+        dataSource?.tableView?(tableView, cancelPrefetchingForRowsAt: [indexPath])
+    }
+
+    var isShowingLoadingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
+    }
+
+    func numberOfRenderedFeedImageViews() -> Int {
+        return tableView.numberOfRows(inSection: feedImageSection)
+    }
+
+    var feedImageSection: Int {
+        0
+    }
+
+    func feedImageView(atIndex index: Int = 0) -> UITableViewCell? {
+        let dataSource = tableView.dataSource
+        let indexPath: IndexPath = .init(row: index, section: 0)
+        return dataSource?.tableView(tableView, cellForRowAt: indexPath)
+    }
+}
---
+++ EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -0,0 +1,13 @@
+//  Copyright © 2021 Andreas Link. All rights reserved.
+
+import UIKit
+
+extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+}
