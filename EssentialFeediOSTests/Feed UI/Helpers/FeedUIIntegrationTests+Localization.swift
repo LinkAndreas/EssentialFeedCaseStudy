cc8d4b2f1363a37599a12f208937e511a2d1fc60
Andreas Link <mail@linkandreas.de>
2021-09-21 21:35:41 +0200

    Move localization strings to presentation module

--- EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+Localization.swift
+++ EssentialFeediOSTests/Feed UI/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -1,13 +1,12 @@
 //  Copyright © 2021 Andreas Link. All rights reserved.
 
-import EssentialFeediOS
-import Foundation
+import EssentialFeed
 import XCTest
 
 extension FeedUIIntegrationTests {
     func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
         let table = "Feed"
-        let bundle = Bundle(for: FeedViewController.self)
+        let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
 
         if value == key {
