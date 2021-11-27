3ac572e9caf4ce756a37616a9747d3b7da99974a
Andreas Link <mail@linkandreas.de>
2021-11-27 09:56:42 +0100

    Add new feed store version adding an optional data property to `ManagedFeedItem`

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -80,6 +80,7 @@ private class ManagedFeedImage: NSManagedObject {
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
     @NSManaged var url: URL
+    @NSManaged var data: Data?
     @NSManaged var cache: ManagedCache
 }
 
