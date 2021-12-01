c17d36104b12f7fc48f144e765f440657a78a38b
Andreas Link <mail@linkandreas.de>
2021-12-01 13:28:34 +0100

    `FeedLoaderWithFallbackComposite.fetchFeed` delivers primary feed on primary loader success

--- EssentialApp/EssentialAppTests/EssentialAppTests.swift
+++ EssentialApp/EssentialAppTests/EssentialAppTests.swift
@@ -5,8 +5,99 @@
 //  Created by Andreas Link on 01.12.21.
 //
 
+import EssentialFeed
 import XCTest
 
-class FeedLoaderWithFallbackTests: XCTestCase {
+class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let primary: FeedLoader
+    private let fallback: FeedLoader
 
+    init(primary: FeedLoader, fallback: FeedLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+
+    func fetchFeed(completion: @escaping (FeedLoader.Result) -> Void) {
+        primary.fetchFeed(completion: completion)
+    }
+}
+
+class FeedLoaderWithFallbackTests: XCTestCase {
+    func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
+        let primaryFeed = uniqueFeed()
+        let fallbackFeed = uniqueFeed()
+        let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
+
+        expect(sut, toCompleteWith: .success(primaryFeed))
+    }
+}
+
+extension FeedLoaderWithFallbackTests {
+    private func makeSUT(
+        primaryResult: FeedLoader.Result,
+        fallbackResult: FeedLoader.Result
+    ) -> FeedLoaderWithFallbackComposite {
+        let primaryLoader = FeedLoaderStub(result: primaryResult)
+        let fallbackLoader = FeedLoaderStub(result: fallbackResult)
+        return FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+    }
+
+    private func uniqueFeed() -> [FeedImage] {
+        return [FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())]
+    }
+
+    private func anyURL() -> URL {
+        return URL(string: "http://any.url")!
+    }
+
+    final class FeedLoaderStub: FeedLoader {
+        private let result: FeedLoader.Result
+
+        init(result: FeedLoader.Result) {
+            self.result = result
+        }
+
+        func fetchFeed(completion: @escaping (FeedLoader.Result) -> Void) {
+            completion(result)
+        }
+    }
+
+    private func expect(
+        _ sut: FeedLoaderWithFallbackComposite,
+        toCompleteWith expectedResult: FeedLoader.Result,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let expectation = expectation(description: "Wait for result.")
+        sut.fetchFeed { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedFeed), .success(expectedFeed)):
+                XCTAssertEqual(
+                    receivedFeed,
+                    expectedFeed,
+                    "Expected to receive \(expectedFeed), but received \(receivedFeed) instead.",
+                    file: file,
+                    line: line
+                )
+
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTFail(
+                    "Expected to receive \(expectedError), but received \(receivedError) instead.",
+                    file: file,
+                    line: line
+                )
+
+            default:
+                XCTFail(
+                    "Expected to receive \(expectedResult), but received \(receivedResult) instead.",
+                    file: file,
+                    line: line
+                )
+            }
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 1.0)
+    }
 }
