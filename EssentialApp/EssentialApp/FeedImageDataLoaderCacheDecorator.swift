b6cde0208c17e0ff65c43fc66a65acb1e09a0fc1
Andreas Link <mail@linkandreas.de>
2021-12-02 15:06:09 +0100

    Move `FeedImageDataLoaderCacheDecorator` to production

---
+++ EssentialApp/EssentialApp/FeedImageDataLoaderCacheDecorator.swift
@@ -0,0 +1,33 @@
+//  Copyright © 2021 Andreas Link. All rights reserved.
+
+import EssentialFeed
+
+public final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    private let decoratee: FeedImageDataLoader
+    private let cache: FeedImageDataCache
+
+    public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+
+    public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case let .success(imageData):
+                self.cache.saveIgnoringResult(imageData, for: url)
+                completion(result)
+
+            case .failure:
+                completion(result)
+            }
+        }
+    }
+}
+
+private extension FeedImageDataCache {
+    func saveIgnoringResult(_ imageData: Data, for url: URL) {
+        save(imageData, for: url) { _ in }
+    }
+}
--- EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -1,38 +1,8 @@
 //  Copyright © 2021 Andreas Link. All rights reserved.
 
+import EssentialApp
 import EssentialFeed
 import XCTest
-
-public final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
-    private let decoratee: FeedImageDataLoader
-    private let cache: FeedImageDataCache
-
-    public init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
-        self.decoratee = decoratee
-        self.cache = cache
-    }
-
-    public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
-        return decoratee.loadImageData(from: url) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case let .success(imageData):
-                self.cache.saveIgnoringResult(imageData, for: url)
-                completion(result)
-
-            case .failure:
-                completion(result)
-            }
-        }
-    }
-}
-
-private extension FeedImageDataCache {
-    func saveIgnoringResult(_ imageData: Data, for url: URL) {
-        save(imageData, for: url) { _ in }
-    }
-}
-
 
 final class FeedImageDataLoaderCacheDecoratorTests: XCTestCase, FeedImageDataLoaderTestCase {
     func test_init_doesNotLoadImageData() {
