8e31c84640a4414e1365fd1d5336e237ce6cb24e
Andreas Link <mail@linkandreas.de>
2021-12-03 14:46:40 +0100

    Add #If DEBUG compiler flags to avoid test code in production

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -20,9 +20,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .defaultDirectoryURL()
             .appendingPathComponent("feed-store.sqlite")
 
+        #if DEBUG
         if CommandLine.arguments.contains("-reset") {
             try? FileManager.default.removeItem(at: localStoreURL)
         }
+        #endif
 
         let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
@@ -51,14 +53,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 extension SceneDelegate {
     private func makeRemoteClient() -> HTTPClient {
-        switch UserDefaults.standard.string(forKey: "connectivity") {
-        case "offline":
+        #if DEBUG
+        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
             return AlwaysFailingHTTPClient()
-        default:
-            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         }
+        #endif
+
+        return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
     }
 
+    #if DEBUG
     final class AlwaysFailingHTTPClient: HTTPClient {
         private class Task: HTTPClientTask {
             func cancel() {}
@@ -69,5 +73,6 @@ extension SceneDelegate {
             return Task()
         }
     }
+    #endif
 }
 
