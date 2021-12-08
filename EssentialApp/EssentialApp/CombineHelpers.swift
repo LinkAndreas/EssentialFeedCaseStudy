14b3dc59e19af195e58fda853e18f35e4441fbd5
Andreas Link <mail@linkandreas.de>
2021-12-08 14:36:04 +0100

    Move Combine helpers to separate file

---
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -0,0 +1,118 @@
+//  Copyright © 2021 Andreas Link. All rights reserved.
+
+import Combine
+import EssentialFeed
+
+public extension FeedImageDataLoader {
+    typealias Publisher = AnyPublisher<Data, Error>
+
+    func loadPublisher(from url: URL) -> Publisher {
+        var task: FeedImageDataLoaderTask?
+        return Deferred {
+            Future { completion in
+                task = self.loadImageData(from: url, completion: completion)
+            }
+        }
+        .handleEvents(receiveCancel: { task?.cancel() })
+        .eraseToAnyPublisher()
+    }
+}
+
+public extension FeedLoader {
+    typealias Publisher = AnyPublisher<[FeedImage], Error>
+
+    func loadPublisher() -> Publisher {
+        Deferred {
+            Future(self.load)
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+private extension FeedCache {
+    func saveIgnoringResult(_ feed: [FeedImage]) {
+        save(feed) { _ in }
+    }
+}
+
+private extension FeedImageDataCache {
+    func saveIgnoringResult(_ data: Data, for url: URL) {
+        save(data, for: url) { _ in }
+    }
+}
+
+extension Publisher where Output == [FeedImage] {
+    func caching(to cache: FeedCache) -> AnyPublisher<Output, Failure> {
+        return handleEvents(receiveOutput: cache.saveIgnoringResult)
+            .eraseToAnyPublisher()
+    }
+}
+
+extension Publisher where Output == Data {
+    func caching(to cache: FeedImageDataCache, using url: URL) -> AnyPublisher<Output, Failure> {
+        return handleEvents(receiveOutput: { cache.saveIgnoringResult($0, for: url) })
+            .eraseToAnyPublisher()
+    }
+}
+
+extension Publisher {
+    func fallback(to fallbackPublisher:  @escaping () -> AnyPublisher<Output, Failure>) -> AnyPublisher<Output, Failure> {
+        self.catch { _ in fallbackPublisher() }.eraseToAnyPublisher()
+    }
+}
+
+extension Publisher {
+    func dispatchOnMainQueue() -> AnyPublisher<Output, Failure> {
+        return receive(on: DispatchQueue.immediateWhenOnMainQueueScheduler).eraseToAnyPublisher()
+    }
+}
+
+extension DispatchQueue {
+    static var immediateWhenOnMainQueueScheduler: ImmediateWhenOnMainQueueScheduler { .init() }
+
+    struct ImmediateWhenOnMainQueueScheduler: Scheduler {
+        typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
+        typealias SchedulerOptions = DispatchQueue.SchedulerOptions
+
+        var now: DispatchQueue.SchedulerTimeType {
+            return DispatchQueue.main.now
+        }
+
+        var minimumTolerance: DispatchQueue.SchedulerTimeType.Stride {
+            return DispatchQueue.main.minimumTolerance
+        }
+
+        func schedule(options: DispatchQueue.SchedulerOptions?, _ action: @escaping () -> Void) {
+            guard Thread.isMainThread else {
+                return DispatchQueue.main.schedule(options: options, action)
+            }
+
+            action()
+        }
+
+        func schedule(
+            after date: DispatchQueue.SchedulerTimeType,
+            tolerance: DispatchQueue.SchedulerTimeType.Stride,
+            options: DispatchQueue.SchedulerOptions?,
+            _ action: @escaping () -> Void
+        ) {
+            DispatchQueue.main.schedule(after: date, tolerance: tolerance, options: options, action)
+        }
+
+        func schedule(
+            after date: DispatchQueue.SchedulerTimeType,
+            interval: DispatchQueue.SchedulerTimeType.Stride,
+            tolerance: DispatchQueue.SchedulerTimeType.Stride,
+            options: DispatchQueue.SchedulerOptions?,
+            _ action: @escaping () -> Void
+        ) -> Cancellable {
+            return DispatchQueue.main.schedule(
+                after: date,
+                interval: interval,
+                tolerance: tolerance,
+                options: options,
+                action
+            )
+        }
+    }
+}
